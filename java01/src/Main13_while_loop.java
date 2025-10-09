1817eef53fdc856d11b47fed144154af28ee2c90
Vaidul1S <vaidas.ad25@gmail.com>
2025-10-09 12:29:56 +0300

    10.09

---
+++ java01/src/Main13_while_loop.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main13_while_loop {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+
+        scanner.close();
+    }
+}
