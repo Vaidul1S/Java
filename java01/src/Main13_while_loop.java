062deca9062be6e10c301095cc266c3e27f5e4b9
Vaidul1S <vaidas.ad25@gmail.com>
2025-10-09 12:39:48 +0300

    10.09

--- java01/src/Main13_while_loop.java
+++ java01/src/Main13_while_loop.java
@@ -4,9 +4,14 @@ public class Main13_while_loop {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
+        String name = "";
 
+        while(name.isEmpty()) {
+            System.out.println("Enter you name");
+            name = scanner.nextLine();
+        }
 
-
+        System.out.println("Hello, " + name);
 
         scanner.close();
     }
