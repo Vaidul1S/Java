5f53a3a2e5fc727b00cdb0e5936c599371e6c74e
Vaidul1S <vaidas.ad25@gmail.com>
2025-10-10 11:45:52 +0300

    10.10

--- java01/src/Main14_for_loop.java
+++ java01/src/Main14_for_loop.java
@@ -19,7 +19,27 @@ public class Main14_for_loop {
             Thread.sleep(500);
         }
 
-        System.out.println("Happy new year!!!");
+        System.out.println("We have a lift off!!!");
+
+        // nested loop
+
+        int rows;
+        int cols;
+        char symbol;
+
+        System.out.print("Enter a number of row: ");
+        rows = scanner.nextInt();
+        System.out.print("Enter a number of columns: ");
+        cols = scanner.nextInt();
+        System.out.print("Enter a symbol to print: ");
+        symbol = scanner.next().charAt(0);
+
+        for (int i = 0; i < cols; i++){
+            for (int j = 0; j < rows; j++){
+                System.out.print(symbol + " ");
+            }
+            System.out.println();
+        }
 
         scanner.close();
     }
