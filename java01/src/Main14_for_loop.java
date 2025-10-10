25e4e4dac717d75dc528bf91d7b30d81e3448006
Vaidul1S <vaidas.ad25@gmail.com>
2025-10-10 11:27:47 +0300

    10.10

--- java01/src/Main14_for_loop.java
+++ java01/src/Main14_for_loop.java
@@ -1,15 +1,25 @@
 import java.util.Scanner;
 
 public class Main14_for_loop {
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
         Scanner scanner = new Scanner(System.in);
 
         String name = "";
 
-        for(int i = 0; i < 10; i++){
+        for(int i = 0; i < 5; i++){
             System.out.printf("%d. Pizza ", i + 1);
+            System.out.println(" ");
         }
 
+        System.out.print("How many times you want to loop?: ");
+        int max = scanner.nextInt();
+
+        for (int i = max; i > 0; i--){
+            System.out.println(i);
+            Thread.sleep(500);
+        }
+
+        System.out.println("Happy new year!!!");
 
         scanner.close();
     }
