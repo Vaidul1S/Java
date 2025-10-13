d439974ec2403e6bc11005946dd94ec13713994c
Vaidul1S <vaidas.ad25@gmail.com>
2025-10-14 00:57:18 +0300

    10.14

--- java01/src/Main17_array.java
+++ java01/src/Main17_array.java
@@ -1,8 +1,10 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main17_array {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         String[] fruits = {"Apple", "Greatfruit", "Orange", "Lime", "Pear"};
 
@@ -13,20 +15,32 @@ public class Main17_array {
         System.out.println(fruits[0]);
 
         Arrays.sort(fruits);
-//        Arrays.fill(fruits, "Pineapple");
+        // Arrays.fill(fruits, "Pineapple");
 
-        for (int i = 0; i < numbOfFruits; i++) {
-            System.out.print(fruits[i] + " ");
-        }
-
-        System.out.println();
-
-        // foreach loop
+        // enhanced loop  or foreach loop
 
         for(String fruit : fruits){
             System.out.println(fruit);
         }
 
+        String[] foods;
+        System.out.print("What number of food do you want?: ");
+        int size = scanner.nextInt();
+        scanner.nextLine();
+
+        foods = new String[size];
+
+        for (int i = 0; i < foods.length; i++){
+            System.out.print("Enter a food: ");
+            foods[i] = scanner.nextLine();
+        }
+
+        for(String food: foods){
+            System.out.print(food + " ");
+        }
+
+
+        scanner.close();
     }
 
 
