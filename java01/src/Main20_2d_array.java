6b1d4eeaa8cd42d06e5c9d3e9aa4f0908fc81777
Vaidul1S <vaidas.ad25@gmail.com>
2025-10-15 11:48:32 +0300

    10.15

--- java01/src/Main20_2d_array.java
+++ java01/src/Main20_2d_array.java
@@ -1,22 +1,45 @@
-import java.util.Scanner;
-
 public class Main20_2d_array {
 
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
 
         String[] fruits = {"Apple", "Greatfruit", "Orange", "Lime", "Pear", "Banana"};
         String[] vegetables = {"Potato", "Onion", "Carrot"};
         String[] meats = {"Chicken", "Beef", "Pork", "Lamb"};
 
-        String[][] groceries = {fruits, vegetables, meats};
+        String[][] groceries1 = {fruits, vegetables, meats};
+        String[][] groceries2 = {{"Apple", "Greatfruit", "Orange", "Lime", "Pear", "Banana"},
+                                {"Potato", "Onion", "Carrot"},
+                                {"Chicken", "Beef", "Pork", "Lamb"}};
 
-        for(String[] foods : groceries){
-            System.out.println(foods);
+        groceries2[1][0] = "Celery";
+
+        for(String[] foods : groceries1){
+            for(String food : foods){
+                System.out.print(food + " ");
+            }
+            System.out.println();
         }
 
-        scanner.close();
+        for(String[] foods : groceries2){
+            for(String food : foods){
+                System.out.print(food + " ");
+            }
+            System.out.println();
+        }
+
+        char[][] telephone = {{'1', '2', '3'},
+                                {'4', '5', '6'},
+                                {'7', '8', '9'},
+                                {'*', '0', '#'}};
+
+        for(char[] row : telephone){
+            for (char number : row){
+                System.out.print(number + " ");
+            }
+            System.out.println();
+        }
+
+        
     }
 
 
