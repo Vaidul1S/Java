7480bb85e3c87645d556222273a30a517197f0b7
Vaidul1S <vaidas.ad25@gmail.com>
2025-10-17 13:58:11 +0300

    10.17

--- java01/src/Main23_array_of_objects.java
+++ java01/src/Main23_array_of_objects.java
@@ -6,16 +6,23 @@ public class Main23_array_of_objects {
 
         Scanner scanner = new Scanner(System.in);
 
-        Game game1 = new Game("Far Cry 53", "Ubisoft");
+        Game game1 = new Game("Far Cry 9", "Ubisoft");
         Game game2 = new Game("Red Alert 5", "Westwood studios");
         Game game3 = new Game("StarCraft World", "Blizzard North");
 
-        Game[] games = {game1, game2, game3};
+        Game[] games1 = {game1, game2, game3};
 
-        for (int i = 0; i < games.length; i++) {
-            games[i].release();
+        for (Game game : games1) {
+            game.release();
         }
 
+        Game[] games2 = {new Game("Assassin Creed 8", "Ubisoft"),
+                new Game("Assassin Creed 9", "Ubisoft"),
+                new Game("Assassin Creed 10", "Ubisoft"),};
+
+        for (Game game : games2) {
+            game.release();
+        }
 
         scanner.close();
 
