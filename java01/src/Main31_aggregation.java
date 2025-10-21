17953b2e5e1ba2a1f2b862a705c3d1afd1f0dc90
Vaidul1S <vaidas.ad25@gmail.com>
2025-10-22 01:04:04 +0300

    10.22

---
+++ java01/src/Main31_aggregation.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main31_aggregation {
+
+    public static void main(String[] args) {
+
+        // Aggregation = Represents a has a relationship between objects
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+        scanner.close();
+
+
+    }
+
+
+
+}
