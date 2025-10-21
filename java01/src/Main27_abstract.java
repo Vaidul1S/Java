d738cad8a9b8cf637cfc5d590114fa957aa13a0a
Vaidul1S <vaidas.ad25@gmail.com>
2025-10-21 10:45:16 +0300

    10.21

---
+++ java01/src/Main27_abstract.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main27_abstract {
+
+    public static void main(String[] args) {
+
+        // method overriding
+
+        Scanner scanner = new Scanner(System.in);
+
+        Circle circle = new Circle(5);
+        Triangle triangle = new Triangle(3,4);
+        Rectangle rectangle = new Rectangle(6,9);
+
+        circle.display();
+
+        System.out.printf("%.1f cm²\n", circle.area());
+        System.out.printf("%.1f cm²\n", triangle.area());
+        System.out.printf("%.1f cm²\n", rectangle.area());
+
+
+
+        scanner.close();
+
+
+    }
+
+
+
+}
