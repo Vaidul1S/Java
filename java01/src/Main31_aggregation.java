98825fb828207b8dc77d4ae1d1732a3df4b0ab1c
Vaidul1S <vaidas.ad25@gmail.com>
2025-10-22 10:41:18 +0300

    10.22

--- java01/src/Main31_aggregation.java
+++ java01/src/Main31_aggregation.java
@@ -8,6 +8,19 @@ public class Main31_aggregation {
 
         Scanner scanner = new Scanner(System.in);
 
+        Book book1 = new Book("A Game of Thrones", 638);
+        Book book2 = new Book("A Clash of Kings", 756);
+        Book book3 = new Book("A Feast for Crows", 728);
+
+        Book[] books = {book1, book2, book3};
+
+        for (Book book : books){
+            System.out.println(book.displayInfo());
+        }
+
+        Library library = new Library("National", 1650, books);
+
+        library.displayInfo();
 
 
 
---
+++ java01/src/Main32_composition.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main32_composition {
+
+    public static void main(String[] args) {
+
+        // Composition - Represents a part of relationship between objects.
+        // Allows complex objects to be constructed from smaller objects.
+
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+
+
+        scanner.close();
+
+
+    }
+
+
+
+}
