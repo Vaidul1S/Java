db18387790d30543c67f26ef0b9bf7a83e2bf606
Vaidul1S <vaidas.ad25@gmail.com>
2025-10-24 01:33:38 +0300

    10.24

---
+++ java01/src/Hangman_Game.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Hangman_Game {
+
+    public static void main(String[] args) {
+
+        // Java Hangman Game
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+        scanner.close();
+
+
+    }
+
+
+
+}
