3d2fc61d4a8ff8bd29ef0080a65cebacef333375
Vaidul1S <vaidas.ad25@gmail.com>
2025-10-23 10:53:06 +0300

    10.23

---
+++ java01/src/Main36_file_writing_reading.java
@@ -0,0 +1,50 @@
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main36_file_writing_reading {
+
+    public static void main(String[] args) {
+
+        // How to write a file using java
+
+        // FileWriter - for small or medium text files
+        // BufferWriter - for large text
+        // PrintWriter - for structured data like reports or logs
+        // FileOutputStream - for binary files like images, audio
+        // There is more
+
+        Scanner scanner = new Scanner(System.in);
+
+        String filePath = "src\\text.txt";
+        String textContent = "I like pizza.\nBut I eat it rarely.\nI don't want to become fat.";
+
+        try(FileWriter writer = new FileWriter(filePath);){
+            writer.write(textContent);
+            System.out.println("File has been written");
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+        } catch (IOException e){
+            System.out.println("Could not write the file");
+        }
+
+        // How to read a file using java
+
+        // BufferedReader - for text files line by line
+        // FileInputStream - for binary
+        // RandomAccessFile - for read/write specific portion of a large file
+
+        
+
+
+
+
+        scanner.close();
+
+
+    }
+
+
+
+}
