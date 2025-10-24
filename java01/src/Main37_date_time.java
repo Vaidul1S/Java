df7fad3ac07b676aa8138c5b3b28b1c4e40248e5
Vaidul1S <vaidas.ad25@gmail.com>
2025-10-24 12:14:51 +0300

    10.24

---
+++ java01/src/Main37_date_time.java
@@ -0,0 +1,47 @@
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Main37_date_time {
+
+    public static void main(String[] args) {
+
+        // Date and time
+
+        Scanner scanner = new Scanner(System.in);
+
+        LocalDate date = LocalDate.now();
+        System.out.println(date);
+
+        LocalDateTime time = LocalDateTime.now();
+        System.out.println(time);
+
+        Instant instant = Instant.now();
+        System.out.println(instant);
+
+        // custom format
+
+        LocalDateTime dateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String newDateTime = dateTime.format(formatter);
+        System.out.println(newDateTime);
+
+        LocalDateTime dateTime1 = LocalDateTime.of(2020, 3, 17, 0, 0, 0);
+        LocalDateTime dateTime2 = LocalDateTime.of(2019, 11, 17, 0, 1, 2);
+        System.out.println(dateTime1);
+        System.out.println(dateTime2);
+
+
+
+
+
+        scanner.close();
+
+
+    }
+
+
+
+}
