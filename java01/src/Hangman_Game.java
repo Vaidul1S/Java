872dd83e5709e24d77cb0ea5186b417b986221e2
Vaidul1S <vaidas.ad25@gmail.com>
2025-10-24 11:32:55 +0300

    10.24

--- java01/src/Hangman_Game.java
+++ java01/src/Hangman_Game.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Hangman_Game {
@@ -8,12 +9,103 @@ public class Hangman_Game {
 
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("\nWelcome to Java Hangman Game\n");
+
+        String word = "pizza";
+
+        ArrayList<Character> wordState = new ArrayList<>();
+        int wrongGuesses = 0;
+
+        for (int i = 0; i < word.length(); i++) {
+            wordState.add('_');
+        }
+
+        while (wrongGuesses < 6){
+            System.out.print("Word: ");
+
+            for (char c : wordState){
+                System.out.print(c + " ");
+            }
+            System.out.println();
+            System.out.println(getHangmanArt(wrongGuesses));
+
+            System.out.print("Guess a letter: ");
+            char guess = scanner.next().toLowerCase().charAt(0);
+
+            if(word.indexOf(guess) >= 0){
+                System.out.println("Correct guess!\n");
+                for (int i = 0; i < word.length(); i++) {
+                    if(word.charAt(i) == guess){
+                        wordState.set(i, guess);
+
+                    }
+                }
+
+                if(!wordState.contains('_')){
+                    System.out.println(getHangmanArt(wrongGuesses));
+                    System.out.println("You win!");
+                    System.out.println("The word was: " + word);
+                    break;
+                }
+            }
+            else {
+                System.out.printf("There is no '%c' letter in a word.\n", guess);
+                wrongGuesses++;
+            }
+        }
+
+        if(wrongGuesses >= 6){
+            System.out.println(getHangmanArt(wrongGuesses));
+            System.out.println("Game over!");
+            System.out.println("The word was " + word);
+        }
 
 
 
         scanner.close();
 
 
+    }
+
+    static String getHangmanArt(int wronguesses){
+        return switch (wronguesses){
+            case 0 -> """
+                    
+                    
+                    
+                    """;
+            case 1 -> """
+                     o
+                    
+                    
+                    """;
+            case 2 -> """
+                     o
+                     |
+                    
+                    """;
+            case 3 -> """
+                     o
+                    /|
+                    
+                    """;
+            case 4 -> """
+                     o
+                    /|\\
+                    
+                    """;
+            case 5 -> """
+                     o
+                    /|\\
+                    /
+                    """;
+            case 6 -> """
+                     o
+                    /|\\
+                    / \\
+                    """;
+            default -> "";
+        };
     }
 
 
