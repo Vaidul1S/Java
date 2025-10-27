e4f6c01745d4c858f7d9b11ca439cdd54e3c37a5
Vaidul1S <vaidas.ad25@gmail.com>
2025-10-27 11:44:48 +0200

    10.27

--- java01/src/Main40_generics.java
+++ java01/src/Main40_generics.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main40_generics {
@@ -5,11 +6,30 @@ public class Main40_generics {
     public static void main(String[] args) {
 
         // Generics - a concept where you can write a class, interface, or method that is compatible with different data types
+        // ArrayList is a generic, <T> T - type
+        // <T, U> U - type tik kad alfabetas toliau butu V ir tt.
 
         Scanner scanner = new Scanner(System.in);
 
-        
+        ArrayList<String> fruits = new ArrayList<>();
 
+        fruits.add("apple");
+        fruits.add("orange");
+        fruits.add("pear");
+        System.out.println(fruits);
+
+        Box<String> box = new Box<>();
+
+        box.setItem("greatfruit");
+        System.out.println(box.getItem());
+
+        Product<String, Double> product1 = new Product<>("banana", 1.49);
+        System.out.println(product1.getItem());
+        System.out.println(product1.getPrice() + " €");
+
+        Product<String, Double> product2 = new Product<>("orange", 0.99);
+        System.out.println(product2.getItem());
+        System.out.println(product2.getPrice() + " €");
 
         scanner.close();
 
---
+++ java01/src/Main41_HashMap.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main41_HashMap {
+
+    public static void main(String[] args) {
+
+        // HashMap - A data structure that stores key-value pairs, keys are unique, values - not
+        // Does not maintain any order
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        scanner.close();
+
+
+    }
+
+
+
+}
