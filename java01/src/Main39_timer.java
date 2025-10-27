d2821b165b49ddcc12439257ac126a81ea7a2f08
Vaidul1S <vaidas.ad25@gmail.com>
2025-10-27 11:10:19 +0200

    10.27

--- java01/src/Main39_timer.java
+++ java01/src/Main39_timer.java
@@ -1,18 +1,33 @@
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Main39_timer {
 
     public static void main(String[] args) {
 
-        // timer - class that schedules tasks at specific times of periodically
+        // Timer - class that schedules tasks at specific times of periodically
         // TimerTask - the task that will executed be the timer
         // yuo will extend the TimeTask class to define your task create a subclass of TimeTask and override run()
 
 
         Scanner scanner = new Scanner(System.in);
 
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            int count = 3;
+            @Override
+            public void run() {
+                System.out.println("Hello, Bebras!");
+                count--;
+                if(count <= 0){
+                    System.out.println("Task completed.");
+                    timer.cancel();
+                }
+            }
+        };
 
-
+        timer.schedule(task, 1000, 1000);
 
 
         scanner.close();
