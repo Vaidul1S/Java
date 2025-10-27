de83a8739d36f5939db1a205cd78dfb23e554697
Vaidul1S <vaidas.ad25@gmail.com>
2025-10-27 12:01:45 +0200

    10.27

--- java01/src/Main41_HashMap.java
+++ java01/src/Main41_HashMap.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main41_HashMap {
@@ -6,10 +7,29 @@ public class Main41_HashMap {
 
         // HashMap - A data structure that stores key-value pairs, keys are unique, values - not
         // Does not maintain any order
+        // <K, V> key values
 
         Scanner scanner = new Scanner(System.in);
 
+        HashMap<String, Double> map = new HashMap<>();
 
+        map.put("ferrari", 450000.0);
+        map.put("aston martin", 200000.0);
+        map.put("lamborgini", 350000.0);
+        map.put("mclaren", 750000.0);
+        map.put("pagani zonda", 1000000.0);
+
+        map.remove("lamborgini");
+
+        System.out.println(map);
+        System.out.println(map.get("mclaren"));
+        System.out.println(map.containsKey("lamborgini"));
+        System.out.println(map.containsValue(1000000.0));
+        System.out.println(map.size());
+
+        for (String key : map.keySet()){
+            System.out.printf("%s: € %,.2f\n", key, map.get(key));
+        }
 
         scanner.close();
 
---
+++ java01/src/Main42_enums.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main42_enums {
+
+    public static void main(String[] args) {
+
+        // Enums - Enumerations special class that represents a fixed set of constants.
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        scanner.close();
+
+
+    }
+
+
+
+}
