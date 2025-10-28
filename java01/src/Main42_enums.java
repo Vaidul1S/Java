9a6ff78825c4740eb71e11816db452e67a74de13
Vaidul1S <vaidas.ad25@gmail.com>
2025-10-28 11:15:44 +0200

    10.28

--- java01/src/Main42_enums.java
+++ java01/src/Main42_enums.java
@@ -6,8 +6,32 @@ public class Main42_enums {
     public static void main(String[] args) {
 
         // Enums - Enumerations special class that represents a fixed set of constants.
+        // more efficient with switches when comparing Strings
 
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a day of the week: ");
+        String response = scanner.nextLine().toUpperCase();
+
+        try{
+            Day day = Day.valueOf(response);
+            System.out.println(day);
+            System.out.println(day.getDayNumber());
+
+            switch (day){
+                case MONDAY,
+                     TUESDAY,
+                     WEDNESDAY,
+                     THURSDAY,
+                     FRIDAY -> System.out.println("It's a workday.");
+                case SATURDAY,
+                     SUNDAY-> System.out.println("It's a weekend.");
+            }
+        }
+        catch (IllegalArgumentException e){
+            System.out.println("Invalid day!");
+        }
+
 
 
 
