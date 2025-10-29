d4b0d6ca9cfc37651c160f6c78024aa104eb61d1
Vaidul1S <vaidas.ad25@gmail.com>
2025-10-29 10:04:35 +0200

    10.29

--- java01/src/AlarmClock.java
+++ java01/src/AlarmClock.java
@@ -1,3 +1,6 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
 public class AlarmClock {
@@ -7,6 +10,22 @@ public class AlarmClock {
         // Java Alarm Clock
 
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to Java Alarm Clock");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalTime alarTime;
+
+        try{
+            System.out.print("Enter an alarm time (HH:mm:ss): ");
+            String inputTime = scanner.nextLine();
+
+            alarTime = LocalTime.parse(inputTime, formatter);
+            System.out.println("Alarm set for " + alarTime);
+        }
+        catch (DateTimeParseException e){
+            System.out.println("Invalid time format!");
+        }
+
 
 
 
